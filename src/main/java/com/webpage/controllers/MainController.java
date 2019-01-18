769b2326356d1fd23f491a593f1dc2a4c0c7cdb6
Nicolas <nyko_1600@hotmail.com>
2019-01-17 22:45:15 -0300

    MVC

---
+++ src/main/java/com/webpage/controllers/MainController.java
@@ -0,0 +1,19 @@
+package com.webpage.controllers;
+
+
+import vista.Inicio;
+
+/**
+ * Created by Nicolas on 16/1/2019.
+ */
+public class MainController {
+
+  public static void main(String[] args) {
+
+
+    //Create a view : to write student details on console
+    Inicio view = new Inicio();
+
+  }
+
+}
